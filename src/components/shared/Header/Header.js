d5803b78bbef0efae8c0a58032df126c244c7ab6
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-15 15:37:40 +0600

    installed heroicons

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../../assets/logo.png'
 
 const Header = () => {
     return (
         <div>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand>
+                        <img src={logo} alt="logo" />
+                    </Navbar.Brand>
+                    <Nav className="ms-auto">
+                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link>Features</Nav.Link>
+                        <Nav.Link>Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
 
         </div>
     );
