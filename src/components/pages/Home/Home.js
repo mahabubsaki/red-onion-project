d14f97c48b11f31464d0fb3146d2630101393d6f
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-15 15:57:46 +0600

    created responsive navbar with bootstrap

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-
+            <h1>hi</h1>
         </div>
     );
 };
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import logo from '../../../assets/logo.png'
+import { ShoppingCartIcon } from '@heroicons/react/solid'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <Navbar bg="light" variant="light">
-                <Container>
-                    <Navbar.Brand>
-                        <img src={logo} alt="logo" />
-                    </Navbar.Brand>
-                    <Nav className="ms-auto">
-                        <Nav.Link>Home</Nav.Link>
-                        <Nav.Link>Features</Nav.Link>
-                        <Nav.Link>Pricing</Nav.Link>
-                    </Nav>
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#"><img src={logo} alt="logo" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="ms-auto my-2 my-lg-0"
+                        >
+                            <Nav.Link>
+                                <ShoppingCartIcon className="mx-" style={{ height: '32px', position: 'relative', top: '6px' }}></ShoppingCartIcon>
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/login"><button className='btn btn-info text-white rounded-pill'>Login</button></Nav.Link>
+                            <Nav.Link as={Link} to="/signup"><button className='btn btn-danger text-white  rounded-pill'>Sign Up</button></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
-
         </div>
     );
 };
