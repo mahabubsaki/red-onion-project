1f998ba04d0a514eb6fc680ada1192eba2daefac
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-15 15:21:13 +0600

    created route for home and setted header on top and footer on bottom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/shared/Header/Header';
+import Footer from './components/shared/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
 
 function App() {
   return (
     <div>
-
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
