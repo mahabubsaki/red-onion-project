4dc5f8e7499dfd6cf0a51d75674b40bbd200bc92
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-26 12:00:37 +0600

    saved total cost and showen on order page

--- src/components/part-components/EachOrder/EachOrder.js
+++ src/components/part-components/EachOrder/EachOrder.js
@@ -9,22 +9,23 @@ const EachOrder = ({ eachOrder, no }) => {
     const [floor, setFloor] = useState('')
     const [area, setArea] = useState('')
     const [mobile, setMobile] = useState('')
+    const [cost, setCost] = useState('')
     const { formInfo, order } = eachOrder || {}
     useEffect(() => {
         if (order) {
             setTotalOrder(Object.values(order).reduce((acc, item) => acc + item, 0))
         }
         if (formInfo) {
-            const { date, time, orderId, floor, area, mobile } = formInfo
+            const { date, time, orderId, floor, area, mobile, cost } = formInfo
             setDate(date)
             setId(orderId)
             setTime(time)
             setFloor(floor)
             setArea(area)
             setMobile(mobile)
+            setCost(cost)
         }
     }, [order, formInfo])
-    console.log(time)
     return (
         <div className="col border border-dark mb-1 order py-2 rounded-3 card">
             <div className="text-center"><span className="px-4 py-2 bg-success rounded-circle">{no}</span></div>
@@ -32,6 +33,8 @@ const EachOrder = ({ eachOrder, no }) => {
             <h3 className="text-center">Status : <span className="text-warning">Pending</span></h3>
             <h3 className="text-center">Ordered on {date} {time}</h3>
             <h3 className="text-center">Total Foods : {totalOrder}</h3>
+            <h3 className="text-center">Total Cost : <span className="text-warning">${cost}</span></h3>
+            <h4>Additional Info : </h4>
             <p>Floor : {floor}</p>
             <p>Area : {area}</p>
             <p>Mobile : {mobile}</p>
