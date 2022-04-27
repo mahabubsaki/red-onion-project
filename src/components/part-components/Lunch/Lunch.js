cbf530a9d1829319fc238e0b946a7cef53979c5a
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-27 17:19:30 +0600

    fecthed specific data from backend instead of data json

--- src/components/part-components/Lunch/Lunch.js
+++ src/components/part-components/Lunch/Lunch.js
@@ -4,13 +4,13 @@ import EachFood from '../EachFood/EachFood';
 const Lunch = () => {
     const [currentCategory, setCurrentCategory] = useState([])
     useEffect(() => {
-        fetch('data.json')
+        fetch('http://localhost:5000/foods')
             .then(res => res.json())
             .then(data => setCurrentCategory(data.filter(c => c.category === 'Lunch')))
     }, [])
     return (
         <div className="row row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-1">
-            {currentCategory.map(food => <EachFood key={food.id} food={food}></EachFood>)}
+            {currentCategory.map(food => <EachFood key={food._id} food={food}></EachFood>)}
         </div>
     );
 };
