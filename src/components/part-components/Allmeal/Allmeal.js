98465bdc25c9a5636a05816200901a0b2ef9b5a0
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-27 17:15:53 +0600

    fetched from backend instead of data json

--- src/components/part-components/Allmeal/Allmeal.js
+++ src/components/part-components/Allmeal/Allmeal.js
@@ -6,13 +6,13 @@ const Allmeal = () => {
     const { pathname } = useLocation()
     const [allMeal, setAllmeal] = useState([])
     useEffect(() => {
-        fetch('data.json')
+        fetch('http://localhost:5000/foods')
             .then(res => res.json())
             .then(data => setAllmeal(data))
     }, [])
     return (
         <div className={`${pathname !== '/' ? 'd-none' : 'row row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-1 my-3'} `}>
-            {allMeal.map(food => <EachFood key={food.id} food={food}></EachFood>)}
+            {allMeal.map(food => <EachFood key={food._id} food={food}></EachFood>)}
         </div>
     );
 };
